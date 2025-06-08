b2ce14354f0b00464b814fea5e7f084c6b834bb4
ashley-koh <36570313+ashley-koh@users.noreply.github.com>
2025-06-08 14:18:51 +0800

    feat: added redis database for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,14 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 3000;
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
 app.route("/").get((req, res) => {
   res.send("Hello, World!");
 });
